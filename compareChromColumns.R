c28779e9429881009c615b3b5298e20b6221b200
azareie <ashkan.zareie@img.cas.cz>
2018-07-16 10:45:21 +0200

    Comparison done.
    Former-commit-id: a2be421759d1840ef02bc867dc54d55dbb92a2a0

--- compareChromColumns.R
+++ compareChromColumns.R
@@ -10,3 +10,7 @@ dplyr::`%>%` -> `%>%`
 
 C18_mzs <- sub(".*M *(.*?) *T.*", "\\1", C18_peaks$X) %>% as.numeric
 C30_mzs <- sub(".*M *(.*?) *T.*", "\\1", C30_peaks$X) %>% as.numeric
+
+mz_tolerance <- 0.05
+C18_mzs[!data.table::inrange(C18_mzs, C30_mzs - mz_tolerance, C30_mzs + mz_tolerance)] -> in_C18_NOT_in_C30
+C30_mzs[!data.table::inrange(C30_mzs, C18_mzs - mz_tolerance, C18_mzs + mz_tolerance)] -> in_C30_NOT_in_C18
